c19298d7f520016b2463d4a472c01ae489a68c22
Mahammad Abasgulu <mahammadabasguliyev@MacBook-Pro.lan>
2024-01-01 13:08:54 -0600

    Singleton - Object

--- src/main/kotlin/Singleton.kt
+++ src/main/kotlin/Singleton.kt
@@ -1,3 +1,6 @@
+/*
+LONGER WAY:
+
 class Database {
 
     companion object {
@@ -13,12 +16,28 @@ class Database {
         }
     }
 }
+ */
+
+// PREFERRED WAY:
+
+object Database {
+    init {
+        println("Database object created")
+    }
+}
 
 fun main(args: Array<String>) {
 
-    val instance = Database.getInstance()
-    val instance2 = Database.getInstance()
+    /*
+        val instance = Database.getInstance()
+        val instance2 = Database.getInstance()
 
-    println(instance) // Database@f6f4d33
-    println(instance2) // Database@f6f4d33 - same
+        println(instance) // Database@f6f4d33
+        println(instance2) // Database@f6f4d33 - same
+     */
+
+    println(Database)   // Database object created
+                        // Database@4a574795
+    println(Database)   // Database@4a574795
+    println(Database)   // Database@4a574795
 }
